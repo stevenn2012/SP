b7eac29fe8385eb031d7ddc4d67311596b14603a
Steven Puerto <2012stevenn@gmail.com>
2016-12-19 10:04:50 -0500

    commit

---
+++ 1.Presentation-Layer/js/home.js
@@ -0,0 +1,6 @@
+//http://www.danstools.com/javascript-obfuscate/index.php
+eval(function(p,a,c,k,e,d){e=function(c){return c};if(!''.replace(/^/,String)){while(c--){d[c]=k[c]||c}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('$(2).1(0(){$("#3").4("6 "+5.7)});',8,8,'function|ready|document|welcomeTitle|html|sessionStorage|Welcome|nameEmployee'.split('|'),0,{}))
+/*$(document).ready(function(){
+ 	$( "#welcomeTitle" ).html("Welcome "+sessionStorage.nameEmployee);
+});
+*/
---
+++ 1.Presentation-Layer/js/login.js
@@ -0,0 +1,48 @@
+//http://www.danstools.com/javascript-obfuscate/index.php
+$(document).ready(function(){
+ 	$(".clsInfo").change(function(){ cleanInfo("loginValidation")});
+});
+
+function cleanInfo (id) {
+	$("#"+id).html("");
+}
+
+function login () {
+	var account = {
+		"user":$('#user').val(),
+		"pass":calcMD5($('#pass').val())
+	};
+	$.ajax({
+		url: loginService,
+		type: 'GET',
+		data: account,
+		async : true,
+		dataTipe: 'JSON',
+		success: function (data) {
+			if(data.access == true){
+				cleanInfo("loginValidation");
+				if (typeof(Storage) !== "undefined") {
+					$('#pass').val("");
+				    sessionStorage.setItem("nameEmployee", data.nameEmployee);
+				    sessionStorage.setItem("username", data.username);
+				    sessionStorage.setItem("loginCode", data.loginCode);
+				    sessionStorage.setItem("rol", data.rol);
+				    window.location.assign('pages/');
+				} else {
+					$('#pass').val("");
+					$('#loginValidation').html("Lo sentimos! tu buscador no soporta Web Storages");
+				}	
+			}else{
+				singOut();
+				$('#pass').val("");
+				$('#loginValidation').html("Usuario y/o contraseña incorrectos");
+			}
+        },
+        error: function(objXMLHttpRequest) {
+        	singOut();
+        	$('#pass').val("");
+        	$('#loginValidation').html("Error de conexión");
+			console.log("error",objXMLHttpRequest);
+		}
+	});
+}
