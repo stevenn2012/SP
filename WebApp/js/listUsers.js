ce28cf62205d7d37b689f774c4e286363f261750
Steven Puerto <2012stevenn@gmail.com>
2016-12-23 14:27:44 -0500

    list users

--- WebApp/js/listUsers.js
+++ WebApp/js/listUsers.js
@@ -17,15 +17,16 @@ function listUsers() {
 			success: function (data) {
 				if(data.validate == "true"){
 					var users = data.users;
-					console.log("Users: "+JSON.stringify(users));
+					//console.log("Users: "+JSON.stringify(users));
 					var content = '<table class="table table-bordered">';
-					content+='<tr><th>id</th><th>Cedula</th><th>Nombre</th><th>Area</th><th>Nombre de usuario</th></tr>';
+					content+='<tr><th>id</th><th>Cedula</th><th>Nombre</th><th>Area</th><th>Roll</th><th>Nombre de usuario</th></tr>';
 					for (var i = 0; i < users.length; i++) {
 						content+='<tr>';
 					    content+='<td>'+users[i].iduser+'</td>';
 					    content+='<td>'+users[i].document+'</td>';
 					    content+='<td>'+users[i].name+'</td>'; 
 					    content+='<td>'+users[i].area+'</td>';
+					    content+='<td>'+users[i].roll+'</td>';
 					    content+='<td>'+users[i].username+'</td>';
   						content+='</tr>';
 					};
