67024f5171addefffa427c4a1eb11d454fe32057
Steven Puerto <2012stevenn@gmail.com>
2016-12-27 10:25:11 -0500

    crear usuarios

---
+++ WebApp/js/client/listClients.js
@@ -0,0 +1,65 @@
+$(document).ready(function(){
+	getUsers();
+	listUsers();
+	$(".filter").keyup(function(){listUsers()});
+});
+
+var users = {};
+function getUsers() {
+	if(sessionStorage.username && sessionStorage.logincode){
+		var accountLogin = {
+			"username":sessionStorage.username,
+			"logincode":sessionStorage.logincode
+		};
+		$.ajax({
+			url: userList,
+			type: 'GET',
+			data: accountLogin,
+			async : false,
+			dataTipe: 'JSON',
+			success: function (data) {
+				if(data.validate == "true"){
+					users = data.users;
+				}else{
+					console.log("No tiene permisos para listar usuarios");
+				}
+	        },
+	        error: function(objXMLHttpRequest) {
+	        	console.log("error",objXMLHttpRequest);
+			}
+		});
+	}else{
+		if(indexPage != window.location && indexPage != window.location+"index.html"){
+			window.location.assign(indexPage);
+		}
+	}
+}
+
+
+function listUsers() {
+	//console.log("Users: "+JSON.stringify(users));
+	var find = ($('.filter').val()).toUpperCase();
+	var content = '<table class="table table-bordered">';
+	content+='<tr><th>Cedula</th><th>Nombre</th><th>Nombre de usuario</th><th>Area</th><th>Roll</th></tr>';
+	var data = "";
+	for (var i = 0; i < users.length; i++) {
+		var user = (users[i].document+users[i].name+users[i].username+users[i].area+users[i].roll).toUpperCase();
+		if(find == "" || user.indexOf(find)!=-1){
+			data+='<tr>';
+			//content+='<td>'+users[i].iduser+'</td>';
+			data+='<td>'+users[i].document+'</td>';
+			data+='<td>'+users[i].name+'</td>';
+			data+='<td>'+users[i].username+'</td>'; 
+			data+='<td>'+users[i].area+'</td>';
+			data+='<td>'+users[i].roll+'</td>';
+		  	data+='</tr>';
+		}
+	};
+	if(data == ""){
+		$('#msfind').html("No se encontraron usuarios");
+	}else{
+		$('#msfind').html("");
+	}
+	content += data+'</table>';
+	$('#lista').html(content);
+}
