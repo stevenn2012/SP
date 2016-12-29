a5f7caf1de027304a9e979d74ffbcb04951a9778
arodrigueze <andresc.rodriguez@correo.usa.edu.co>
2016-12-29 12:31:18 -0500

    validacion update and insert

--- 2.Logic-Layer/PresupuestosServices/src/logic/App.java
+++ 2.Logic-Layer/PresupuestosServices/src/logic/App.java
@@ -6,14 +6,6 @@ import java.math.BigInteger;
 public class App {
 
 	public static void main(String[] args) {
-		
-		BigInteger a = new BigInteger("100000010101019879879801001");
-		BigDecimal e = new BigDecimal(a);
-								 
-		int  c = 1000000000;
-		long b = Long.parseLong("9223372036854775807");
-		double f = 1231912931923123.123123124;
-		System.out.println(b);
-		//System.out.println(MD5Encryption.getMD5("123456"));
+	System.out.println(MD5Encryption.getMD5("1234"));
 	}
 }
