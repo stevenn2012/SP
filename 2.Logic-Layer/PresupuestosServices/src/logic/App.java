f280fc61fb4dbdc3fbfe4ec4e8ac2985790e27ae
arodrigueze <andresc.rodriguez@correo.usa.edu.co>
2016-12-29 22:22:20 -0500

    db1

--- 2.Logic-Layer/PresupuestosServices/src/logic/App.java
+++ 2.Logic-Layer/PresupuestosServices/src/logic/App.java
@@ -1,11 +1,14 @@
 package logic;
 
-import java.math.BigDecimal;
-import java.math.BigInteger;
+
+
 
 public class App {
 
 	public static void main(String[] args) {
-	System.out.println(MD5Encryption.getMD5("1234"));
+	//System.out.println(MD5Encryption.getMD5("1234"));
+		
+		System.out.println(ProviderLogic.getProvidersJSON());
+		
 	}
 }
