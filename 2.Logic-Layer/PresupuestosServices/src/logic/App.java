050a006159c0e71c8e68edfc18bb90616d133c47
arodrigueze <andresc.rodriguez@correo.usa.edu.co>
2017-01-03 09:22:32 -0500

    add

--- 2.Logic-Layer/PresupuestosServices/src/logic/App.java
+++ 2.Logic-Layer/PresupuestosServices/src/logic/App.java
@@ -10,12 +10,12 @@ import vo.Provider;
 public class App {
 
 	public static void main(String[] args) {
-	//System.out.println(MD5Encryption.getMD5("1234"));
+	System.out.println(MD5Encryption.getMD5("1234"));
 	//System.out.println(LogicProvider.getProvidersJSON());
 		//System.out.println(LogicCountry.getCountriesJSON());
 		//System.out.println(LogicCountry.createCountry(new Country(0, "+075", "Peru")));
-		System.out.println(LogicProvider.getProvidersJSON());
-		System.out.println(LogicProvider.updateProvider(new Provider(1, "10000003", "Super Papas", "descripcion papas ricas")));
+		//System.out.println(LogicProvider.getProvidersJSON());
+		//System.out.println(LogicProvider.updateProvider(new Provider(1, "10000003", "Super Papas", "descripcion papas ricas")));
 
 	}
 }
